60f782386e5462256abba7244930b3b53528f1ef
brachiel <brachiel@github.com>
2016-12-31 18:52:02 +0100

    Implement one level application with sigmoid activation function.

--- src/Main.hs
+++ src/Main.hs
@@ -2,3 +2,14 @@ module Main where
 
 import NeuroNet
 
+-- Some sandbox objects to play around
+bs = createBias [1,2,3,4,1]
+wts = createWeights 5 3 [5,2,6,2,4,3,6,21,7,3,1,56,3,2,4]
+input = createInput [4,2,1]
+
+level = Level {weights=wts, bias=bs}
+output = applyLevel level input
+
+main :: IO ()
+main = do putStrLn $ show output
+
--- src/NeuroNet.hs
+++ src/NeuroNet.hs
@@ -4,19 +4,43 @@ import Data.Matrix as M
 
 type DMatrix = M.Matrix Double
 
-type Matrix = DMatrix
+type Weights = DMatrix
+type Bias = DMatrix
 type Input = DMatrix
 type Output = DMatrix
 
-data Level = Level {weights::DMatrix, bias::DMatrix}
+data Level = Level {weights::Weights, bias::DMatrix}
 type Net = [Level]
 
 
 createInput :: [Double] -> Input
 createInput list = M.fromList (length list) 1 list
 
-applyLevel :: Input -> Level -> Output
-applyLevel input level = M.elementwise (+) a b
+createBias :: [Double] -> Bias
+createBias = createInput
+
+createWeights :: Int -> Int -> [Double] -> Weights
+createWeights rows cols wts = M.fromList rows cols wts
+
+-- | The sigmoid activation function, a standard activation function defined
+--   on the range (0, 1).
+sigmoid :: (Floating a) => a -> a
+sigmoid t = 1 / (1 + exp (-1 * t))
+
+-- | The derivative of the sigmoid function conveniently can be computed in
+--   terms of the sigmoid function.
+sigmoid' :: (Floating a) => a -> a
+sigmoid' t = s * (1 - s)
+              where s = sigmoid t
+
+sigmoidO :: Output -> Output
+sigmoidO out = fmap sigmoid out
+
+applyLevel :: Level -> Input -> Output
+applyLevel l = sigmoidO . preApplyLevel l
+
+preApplyLevel :: Level -> Input -> Output
+preApplyLevel level input = M.elementwise (+) a b
                          where a = M.multStd2 (weights level) input
                                b = (bias level)
 
